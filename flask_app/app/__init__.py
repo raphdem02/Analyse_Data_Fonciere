df2fab3143c0c1027366ba4bcee075af9c118c90
raphdem02 <raph.Demare@gmail.com>
2023-05-09 15:37:47 +0200

    add of the other years

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -11,19 +11,19 @@ def index():
 
 @app.route('/2018/')
 def data2017():
-    return render_template('2017.html')
+    return render_template('2018.html')
 
 @app.route('/2019/')
 def data2018():
-    return render_template('2018.html')
+    return render_template('2019.html')
 
 @app.route('/2020/')
 def data2019():
-    return render_template('2019.html')
+    return render_template('2020.html')
 
 @app.route('/2021/')
 def data2020():
-    return render_template('2020.html')
+    return render_template('2021.html')
 
 @app.route('/2022/')
 def data2021():
